de0337a399563aa90cb093cc953455e8621e7e5c
lwyhasacat <75380531+lwyhasacat@users.noreply.github.com>
2023-04-23 19:44:15 -0400

    Update int_ring.cpp

--- int_ring.cpp
+++ int_ring.cpp
@@ -3,16 +3,14 @@
 
 int main(int argc, char *argv[]) {
     int rank, size, N;
+    int value = 0;
     double start_t, end_t;
-
-    const int array_size = 1000000; // set the size of the array
-    double value[array_size] = {}; // initialize the array with zeros
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc != 2) {
+    if(argc != 2) {
         printf("error: not enough input");
         exit(1);
     }
@@ -20,22 +18,17 @@ int main(int argc, char *argv[]) {
         N = std::stoi(argv[1]);
     }
 
-
     start_t = MPI_Wtime();
 
     for(int i = 0; i < N; i++) {
         if(rank == 0) {
             MPI_Send(value, array_size, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
             MPI_Recv(value, array_size, MPI_DOUBLE, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int j = 0; j < array_size; j++) {
-                value[j] += rank;
-            }
+            value += rank;
         } 
         else{
             MPI_Recv(value, array_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int j = 0; j < array_size; j++) {
-                value[j] += rank;
-            }
+            value += rank;
             MPI_Send(value, array_size, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
         }
     }
