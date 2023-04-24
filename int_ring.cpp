30a2a637bbb7d41268cfc534cf9aed258eaaf125
lwyhasacat <75380531+lwyhasacat@users.noreply.github.com>
2023-04-23 22:47:52 -0400

    Update int_ring.cpp

--- int_ring.cpp
+++ int_ring.cpp
@@ -3,11 +3,12 @@
 
 int main(int argc, char *argv[]) {
     int rank, size, N;
+    int value = 0;
     double start_t, end_t;
 
-    // below for array
-    const int array_size = 500000; // about 2 mbytes
-    double value[array_size] = {}; // initialize the array with zeros
+//     // below for array
+//     const int array_size = 500000; // about 2 mbytes
+//     double value[array_size] = {}; // initialize the array with zeros
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -23,119 +24,46 @@ int main(int argc, char *argv[]) {
 
     start_t = MPI_Wtime();
 
-    // for array 
-    for(int i = 0; i < N; i++) {
-        if(rank == 0) {
-            MPI_Send(value, array_size, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(value, array_size, MPI_DOUBLE, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        } 
-        else{
-            MPI_Recv(value, array_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(value, array_size, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
+//     // for array 
+//     for(int i = 0; i < N; i++) {
+//         if(rank == 0) {
+//             MPI_Send(value, array_size, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+//             MPI_Recv(value, array_size, MPI_DOUBLE, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//         } 
+//         else{
+//             MPI_Recv(value, array_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//             MPI_Send(value, array_size, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
+//         }
+//     }
+
+    // for integer
+    for (int i = 0; i < N; i++) {
+        if (rank == 0) {
+            MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(&value, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            value += rank;
+        } else {
+            MPI_Recv(&value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            value += rank;
+            MPI_Send(&value, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
         }
     }
 
     end_t = MPI_Wtime();
 
-    // for array
-    double bandwidth=N*array_size*sizeof(int)/(end_t-start_t)/1e9;
+//     // for array
+//     double bandwidth = N * array_size * sizeof(int)/(end_t - start_t)/1e9;
+//     if (rank == 0) {
+//         std::cout << "total time: " << end_t - start_t << " seconds" << std::endl;
+//         std::cout << "bandwidth calculated: " << bandwidth <<  " GB/s" << std::endl;
+//     }
+
+    // for int
     if (rank == 0) {
-        std::cout << "total value after " << N << " loops: " << value[0] << std::endl;
+        std::cout << "total value after " << N << " loops: " << value << std::endl;
         std::cout << "total time: " << end_t - start_t << " seconds" << std::endl;
-        std::cout << "bandwidth calculated: " << bandwidth <<  " GB/s" << std::endl;
     }
 
     MPI_Finalize();
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <iostream>
-// #include <mpi.h>
-
-// int main(int argc, char *argv[]) {
-//     int rank, size, N;
-//     // int value = 0;
-//     double start_t, end_t;
-
-//     // below for array
-//     const int array_size = 500000; // about 2 mbytes
-//     double value[array_size] = {}; // initialize the array with zeros
-
-//     MPI_Init(&argc, &argv);
-//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-//     if(argc != 2) {
-//         printf("error: not enough input");
-//         exit(1);
-//     }
-//     else{
-//         N = std::stoi(argv[1]);
-//     }
-
-//     start_t = MPI_Wtime();
-
-//     // for array 
-//     // for(int i = 0; i < N; i++) {
-//     //     if(rank == 0) {
-//     //         MPI_Send(value, array_size, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
-//     //         MPI_Recv(value, array_size, MPI_DOUBLE, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//     //         for(int j = 0; j < array_size; j++) {
-//     //             value[j] += rank;
-//     //         }
-//     //     } 
-//     //     else{
-//     //         MPI_Recv(value, array_size, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//     //         for(int j = 0; j < array_size; j++) {
-//     //             value[j] += rank;
-//     //         }
-//     //         MPI_Send(value, array_size, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
-//     //     }
-//     // }
-
-//     // for integer
-//     for (int i = 0; i < N; i++) {
-//         if (rank == 0) {
-//             MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-//             MPI_Recv(&value, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//             value += rank;
-//         } else {
-//             MPI_Recv(&value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//             value += rank;
-//             MPI_Send(&value, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
-//         }
-//     }
-
-//     end_t = MPI_Wtime();
-
-//     // for array
-//     // double bandwidth=(2.0 * array_size * sizeof(int)) / (1024 * 1024 * (end_t - start_t));
-//     // if (rank == 0) {
-//     //     std::cout << "total value after " << N << " loops: " << value[0] << std::endl;
-//     //     std::cout << "total time: " << end_t - start_t << " seconds" << std::endl;
-//     //     std::cout << "bandwidth calculated: " << bandwidth <<  " MB/s" << std::endl;
-//     // }
-
-//     // for int
-//     if (rank == 0) {
-//         std::cout << "total value after " << N << " loops: " << value << std::endl;
-//         std::cout << "total time: " << end_t - start_t << " seconds" << std::endl;
-//     }
-
-//     MPI_Finalize();
-//     return 0;
-// }
